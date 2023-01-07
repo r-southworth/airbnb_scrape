f0226789f55e7f32c56e7d34a4b0ec79576cf1b3
r-southworth <rachael.southworth@datagrove.com>
2023-01-07 13:55:18 -0500

    still broken

--- tests/airbnb_scrape.spec.ts
+++ tests/airbnb_scrape.spec.ts
@@ -9,79 +9,32 @@ test('get listings', async ({ page }) => {
   await page.getByTestId('structured-search-input-field-query').fill('Costa Rica');
   await page.getByTestId('option-0').getByText('Costa Rica').click();
   await page.getByTestId('structured-search-input-search-button').click();
-  var nextpage = await page.$$eval('._833p2h', numpages =>{
-       const nums: any [] = [];
-       numpages.forEach(num =>{
-        var pagenum: number = +num.innerHTML;
-        nums.push({pagenum});
-       });
-       return nums;
+  await page.waitForLoadState('networkidle');
+
+  const listings = await page.$$eval('.cy5jw6o.dir.dir-ltr', all_items => {
+  const data: any [] = [];
+  all_items.forEach(listing =>{
+    const id = listing.querySelector('.t1jojoys.dir.dir-ltr')?.id;
+    data.push({id});
+  });
+  return data;
+});
+  await page.getByRole('link', { name: 'Next' }).click();
+  while (nextpage) {
+    await page.getByRole('link', { name: 'Next' }).click();
+    await page.waitForLoadState('networkidle');
+    const listings = await page.$$eval('.cy5jw6o.dir.dir-ltr', all_items => {
+      const data: any [] = [];
+      all_items.forEach(listing =>{
+        const id = listing.querySelector('.t1jojoys.dir.dir-ltr')?.id;
+        data.push({id});
+      });
+      return data;
     });
+    nextpage = await page.getByRole('button', { name: 'Next' });
     console.log(nextpage);
-//   const nextpage = await page.$$eval('._833p2h', numpages => {
-//     const nums: any [] = [];
-//     numpages.forEach(num =>{
-//      var pagenum: number = +num.innerHTML;
-//      nums.push({pagenum});
-//     })
-//     const totalpages = nums.sort[(nums.length - 1)];
-//     for (let i=0; i <= totalpages; i++){
-//       await page.getByRole('link', { name: 'Next' }).click();
-//         listings = await page.$$eval('.cy5jw6o.dir.dir-ltr', all_items => {
-//           const data: any [] = [];
-//           all_items.forEach(listing =>{
-//             const id = listing.querySelector('.t1jojoys.dir.dir-ltr')?.id;
-//             data.push({id});
-//           });
-//           return data;
-//         });
-//      }
-//  });
-
-//  var listings: any [] = []
-//  const totalpages = await nextpage.sort[(nextpage.length - 1)]
-//  for (let i=0; i <= totalpages; i++){
-//   await page.getByRole('link', { name: 'Next' }).click();
-//     listings = await page.$$eval('.cy5jw6o.dir.dir-ltr', all_items => {
-//       const data: any [] = [];
-//       all_items.forEach(listing =>{
-//         const id = listing.querySelector('.t1jojoys.dir.dir-ltr')?.id;
-//         data.push({id});
-//       });
-//       return data;
-//     });
-//  }
-
-//   const listings = await page.$$eval('.cy5jw6o.dir.dir-ltr', all_items => {
-//   const data: any [] = [];
-//   all_items.forEach(listing =>{
-//     const id = listing.querySelector('.t1jojoys.dir.dir-ltr')?.id;
-//     data.push({id});
-//   });
-//   return data;
-// });
-  // var nextpage = await page.$$eval('._833p2h', numpages =>{
-  //    const nums: any [] = [];
-  //    numpages.forEach(num =>{
-  //     var pagenum: number = +num.innerHTML;
-  //     nums.push({pagenum});
-  //    })
-  // });
-  // console.log(nextpage.isEnabled());
-  // while (nextpage.isEnabled()) {
-  //   await page.getByRole('link', { name: 'Next' }).click();
-  //   const listings = await page.$$eval('.cy5jw6o.dir.dir-ltr', all_items => {
-  //     const data: any [] = [];
-  //     all_items.forEach(listing =>{
-  //       const id = listing.querySelector('.t1jojoys.dir.dir-ltr')?.id;
-  //       data.push({id});
-  //     });
-  //     return data;
-  //   });
-  //   //nextpage = await page.getByRole('button', { name: 'Next' });
-  //   console.log(nextpage.isEnabled());
-  // }
-//console.log(listings);
+  }
+console.log(listings);
 
 });
 
