858a6ab8ba3dced376f8c8a3bfb7643f01a33ac2
r-southworth <rachael.southworth@datagrove.com>
2023-01-07 14:25:46 -0500

    First two pages working sort of

--- tests/airbnb_scrape.spec.ts
+++ tests/airbnb_scrape.spec.ts
@@ -10,19 +10,25 @@ test('test', async ({ page }) => {
   await page.getByTestId('option-0').getByText('Costa Rica').click();
   await page.getByTestId('structured-search-input-search-button').click();
 
+  const listingimage = page.locator('._6tbg2q');
+  await expect(listingimage.first()).toBeVisible();
   const listings = await page.$$eval('.cy5jw6o.dir.dir-ltr', all_items => {
-  const data: any [] = [];
-  all_items.forEach(listing =>{
-    const id = listing.querySelector('.t1jojoys.dir.dir-ltr')?.id;
-    data.push({id});
+    const data: any [] = [];
+    all_items.forEach(listing =>{
+      const id = listing.querySelector('.t1jojoys.dir.dir-ltr')?.id;
+      data.push({id});
+    });
+    return data;
   });
-  return data;
-});
-  var nextpage = await page.getByRole('link', { name: 'Next' });
-  console.log(nextpage);
-  while (nextpage) {
+  
+  var nextPage = await page.getByRole('link', { name: 'Next' });
+  //var nextPageButton = await nextpage[0];
+
+  console.log(nextPage);
+  if (await nextPage.isEnabled) {
     await page.getByRole('link', { name: 'Next' }).click();
-    const listings = await page.$$eval('.cy5jw6o.dir.dir-ltr', all_items => {
+    await expect(listingimage.first()).toBeVisible();
+    const newlistings = await page.$$eval('.cy5jw6o.dir.dir-ltr', all_items => {
       const data: any [] = [];
       all_items.forEach(listing =>{
         const id = listing.querySelector('.t1jojoys.dir.dir-ltr')?.id;
@@ -30,8 +36,9 @@ test('test', async ({ page }) => {
       });
       return data;
     });
-    nextpage = await page.getByRole('link', { name: 'Next' });
-    console.log(nextpage);
+    // nextpage = await page.getByRole('link', { name: 'Next' });
+    // console.log(nextpage);
+    listings.push(newlistings);
   }
 console.log(listings);
 
